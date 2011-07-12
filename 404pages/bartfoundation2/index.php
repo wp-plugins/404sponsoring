0810206145712dbb1f0b8e0e90e8c9b56c6a05b1
jeroensmeets <jeroensmeets@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-12 12:45:55 +0000

    tagging version 1.3
    git-svn-id: https://plugins.svn.wordpress.org/404sponsoring/tags/1.3@408710 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 404pages/bartfoundation2/index.php
@@ -0,0 +1,84 @@
+<?php
+/*
+404 Name: Bart Foundation 2
+Version: 1.0
+*/
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Bart Foundation: 404 - niet gevonden</title>
+<style>
+html {
+  margin: 0;
+  padding: 0;  
+}
+
+body {
+  margin: 0px;
+  padding: 0px;
+  font-size: 12px;
+  font-family: arial, helvetica; 
+}
+
+h3 {
+  margin: 0px;
+  padding: 0px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  font-size: 30px;
+  line-height: 38px;
+  margin-left: 130px;
+  letter-spacing: 1px;
+}
+
+h4 {
+  margin: 0px;
+  padding: 0px;
+  font-size: 50px;
+  color: #ffe9a8;
+  margin-top: 140px;
+  margin-left: 485px;
+}
+
+#container {
+  width: 908px;
+  height: 635px;
+  margin: 0 auto;
+}
+
+#main {
+  background-image: url('<?php echo DIR404PAGES; ?>background.jpg');
+  background-repeat: no-repeat;
+  width: 908px;
+  height: 635px;
+}
+
+#main p {
+  width: 328px;
+  margin-left: 49px;
+  margin-top: 25px;
+  padding-left: 10px;
+  padding-right: 10px;
+  font-size: 14px;
+  line-height: 18px;
+}
+
+</style>
+<head>
+<body>
+<div id="container">
+  <h3>ZO! JE WILT NAAR PLAATSEN DIE<br />GEWOONLIJK NIET TOEGANKELIJK ZIJN...</h3>
+    <div id="main">
+      <h3 style="padding-top: 27px;">... MISSCHIEN BEN JIJ DAN DE BIKKEL <br />DIE WIJ ZOEKEN!</h3>
+      <p>De pagina, de plek internet waarna je zocht is niet gevonden. Heeft misschien wel nooit bestaan.
+      Controleer voor de zekerheid of het adres op de juiste manier is overgenomen. Of ga anders naar de <a href="<?php echo SITEHOME; ?>">homepage</a> van deze site
+      <br /><br />
+      Voor jongeren met een levensbepalende lichamelijke beperking lijkt toegang tot de zakelijke wereld vaak onbereikbaar. De
+      Bart de Graaff Foundation doet hier iets aan door deze Bikkels bij de start van hun bedrijf te koppelen aan toekomstige
+      concurrenten. Misschien ben je zo'n Bikkel of wil je zo'n jonge ondernemer toegang geven tot jouw zakelijke wereld.
+      <br />Kijk voor meer informatie op <a href="http://www.bartfoundation.nl/">onze site</a>.</p>
+      <h4>404 PAGINA</h4>
+    </div>                                   
+</div>
+</body>
---
+++ 404pages/villaticca/index.php
@@ -0,0 +1,106 @@
+<?php
+/*
+404 Name: Villa Ticca
+Version: 1.0
+*/
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>404 Sponsoring: Villa Tica</title>
+<style>
+
+body, p, a, img {
+  margin: 0px;
+  padding: 0px;
+  border: 0px;
+}
+ 
+a {
+  color: #603177;
+  text-decoration: underline;
+}
+
+.left-text a:hover {
+  color: #fff;
+}
+
+.left-text a {
+  color: #eb9800;
+  text-decoration: none;  
+}
+     
+p {
+  padding-bottom: 15px;
+  line-height: 1.4em;
+}
+     
+#container {
+  margin: 0 auto;
+  width: 1024px;
+  height: 768px;
+  background-image: url('<?php echo DIR404PAGES; ?>background.png'); 
+  background-position: bottom;
+  background-repeat: no-repeat;
+  position: relative;
+}
+
+.koe {
+  position: absolute;
+  bottom: 122px;
+  right: 0px;
+}
+
+.left-text {
+  position: absolute;
+  top: 265px;
+  left: 20px;
+  font-size: 20px;
+  font-family: arial;
+  color: #eb9800;
+  width: 420px;
+}
+
+.bottom-text {
+  position: absolute;
+  bottom: 0px;
+  left: 20px;
+  font-size: 22px;
+  font-family: arial;
+  color: #000;
+  width: 800px;
+}
+
+.logo {
+  position: absolute;
+  bottom: 2px;
+  right: 7px;
+}
+
+</style>
+</head>
+<body>
+<div id="container">
+  <div class="title" align="center"><img src="<?php echo DIR404PAGES; ?>titel.png" /></div>
+  <div class="koe"><img src="<?php echo DIR404PAGES; ?>koe.png" /></div>
+  <div class="left-text">
+  <p>Zeker niet de pagina die u had verwacht.
+Maar als u dan toch hier bent willen we u 
+graag kennis laten maken met <a href="http://www.villaticca.com" target="_blank">Villa Ticca</a>.</p>
+  <p>Villa Ticca is een kinderopvang in Ecuador 
+met 2 locaties, een in Quito, de hoofdstad 
+van Ecuador, en een in Peguche, een 
+Indianengemeenschap vlakbij Otavalo.</p>
+<p>Villa Ticca biedt  aan 110 kansarme kinderen 
+van alleenstaande en minderjarige moeders 
+een veilige plek.</p>
+  </div>
+  <div class="bottom-text">
+  <p>Dit is een zogenaamde 404 pagina. U bent hiernaar verwezen omdat de pagina 
+welke u zocht niet bestaat. <a href="<?php echo SITEHOME; ?>">Klik hier</a> om naar de homepage van deze site te 
+gaan of klik op het logo van Villa Ticca om meer over het goede doel te weten.</p>
+  </div>
+  <div class="logo"><a href="http://www.villaticca.com" target="_blank"><img src="<?php echo DIR404PAGES; ?>logo.png" /></a></div>
+</div>
+</body>
+</html>
